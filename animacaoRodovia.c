e4bd2cf374110153b16e77bf8cb3fd748a505596
JessicaCaroline7 <33902550+JessicaCaroline7@users.noreply.github.com>
2017-11-22 12:54:34 -0200

    Update animacaoRodovia.c

--- animacaoRodovia.c
+++ animacaoRodovia.c
@@ -1,91 +1,60 @@
-#ifdef __APPLE__
-#include <OpenGL/gl.h>
-#include <GLUT/glut.h>
-#else
-#include <GL/gl.h>
-#include <glut.h>
-#endif
+# ifdef __APPLE__
+# include< OpenGL/gl.h>
+# include< GLUT/glut.h>
+# else
+# include<GL/gl.h>
+# include<GL/glut.h>
+# endif
 
-/*
 #include <stdlib.h>
-#include <math.h>
-#include <stdio.h>
 
-#  include <GL/GLUT.h>
-*/
+void setup(void){
+  glClearColor(1.0, 1.0, 1.0, 0.0);
+  glOrtho (0, 500, 0, 500, -1 ,1);
+}
+
 void drawScene(void)
 {
+  glClear(GL_COLOR_BUFFER_BIT);
 
-	glClear(GL_COLOR_BUFFER_BIT);
-	glColor3f(0, 0, 0);
-	glBegin(GL_LINE);
-	
-	glVertex2d(0, 0);
-	glVertex2d(230, 200);
-	glEnd();
+ 
+  glColor3f(0.0, 0.0, 0.0);
+  glLineWidth(15.0);
+  glBegin(GL_LINES);
+  glVertex2i(0,0);  glVertex2i(190,200);
+  glVertex2i(500,0); glVertex2i(300,200);
+  glEnd();
 
+  glBegin(GL_LINES);
+  glVertex2i(250,0);  glVertex2i(250,50);
+  glVertex2i(250,100);  glVertex2i(250,150);
+  glEnd();
 
-	glBegin(GL_LINE);
+  
+  glFlush();
+  glutSwapBuffers();
+ }
 
-	glVertex2d(500, 0);
-	glVertex2d(270, 200);
-	glEnd();
-	glFlush();
+void keyboard(unsigned char key, int x, int y){
+  switch (key) {
+  case 27:
+	exit(0);
+	break;
+  }
 }
 
-
-void setup(void)
+int main(int argc, char** argv)
 {
-	glClearColor(1.0, 1.0, 1.0, 0.0);
-}
+  glutInit(&argc, argv);
 
-void resize(int w, int h)
-{
-	glViewport(0, 0, w, h);
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	glFrustum(-5.0, 5.0, -5.0, 5.0, 5.0, 100.0);
-
-	glMatrixMode(GL_MODELVIEW);
-}
-
-void keyInput(unsigned char key, int x, int y)
-{
-	
-}
-
-void specialKeyInput(int key, int x, int y)
-{
-	/*
-	if (key == GLUT_KEY_LEFT) animationPeriod += 5;
-	if (key == GLUT_KEY_RIGHT) if (animationPeriod > 5) animationPeriod -= 5;
-	glutPostRedisplay();
-	*/
-}
-
-void printInteraction(void)
-{
-	printf("Interaction:");
-	printf("Press space to toggle between animation on and off.");
-	printf("Press the up/down arrow keys to speed up/slow down animation.");
-	printf("Press the x, X, y, Y, z, Z keys to rotate the scene.");
-}
-
-int main(int argc, char **argv)
-{
-	printInteraction();
-	
-	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA);
-	glutInitWindowSize(500, 500);
-	glutInitWindowPosition(100, 100);
-	glutCreateWindow("TOPGEAR2017.cpp");
-	glutDisplayFunc(drawScene);
-	glutReshapeFunc(resize);
-	//glutKeyboardFunc(keyInput);
-	//glutSpecialFunc(specialKeyInput);
-
-	setup();
-
-	glutMainLoop();
+  glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+  glutInitWindowSize (500, 500);
+  glutInitWindowPosition (100, 100); 
+  glutCreateWindow("TOPGEAR2017.cpp");
+  setup();
+  glutDisplayFunc(drawScene);
+  glutKeyboardFunc(keyboard);
+  
+  glutMainLoop();
+  
 }

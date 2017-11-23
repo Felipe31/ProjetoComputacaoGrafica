98a03d88787ba16da274a57ca7bbfc0e40197616
JessicaCaroline7 <33902550+JessicaCaroline7@users.noreply.github.com>
2017-11-23 07:26:27 -0200

    Update animacaoRodovia.c

--- animacaoRodovia.c
+++ animacaoRodovia.c
@@ -34,6 +34,68 @@ void drawScene(void)
   glFlush();
   glutSwapBuffers();
  }
+/*void drawScene(void)
+{                     
+  glClear(GL_COLOR_BUFFER_BIT);
+  //Céu
+  glColor3f(0.68, 0.85, 0.9);
+  glBegin(GL_QUADS);
+  glVertex2i(0,200); glVertex2i(0,500);
+  glVertex2i(500,500); glVertex2i(500,500);
+  glVertex2i(500,200);glVertex2i(500,500);
+  glVertex2i(0,200); glVertex2i(500,200);
+  glEnd();
+ 
+  //pista
+  glColor3f(0.66, 0.66, 0.66); 
+  glBegin(GL_QUADS);
+  glVertex2i(0,0);  glVertex2i(190,200);
+  glVertex2i(300,200); glVertex2i(300,200);
+  glVertex2i(500,0); glVertex2i(300,200);
+  glVertex2i(0,0); glVertex2i(500,0);
+  glEnd();
+  
+  //Passarela
+  glColor3f(0.5,0.5,0.5);
+  glBegin(GL_POLYGON);
+  glVertex2i(100,210); glVertex2i (100,220); glVertex2i(400,210); glVertex2i(400,220);
+  glEnd();
+  glColor3f(0.66,0.66,0.66);
+  glBegin(GL_QUADS);
+  glVertex2i(100,200); glVertex2i (100,220); glVertex2i(120,220); glVertex2i(120,200);
+  glVertex2i(380,200); glVertex2i (380,220); glVertex2i(400,220); glVertex2i(400,200);
+  glEnd();
+  
+  //placa
+  glColor3f(1.0,1.0,0.0);
+  glBegin(GL_QUADS);
+  glVertex2i(420, 120); glVertex2i(440,140); glVertex2i(460,120); glVertex2i(440,100);
+  glEnd();
+
+  glColor3f(0.72, 0.45, 0.2);
+  glBegin(GL_LINES);
+  glVertex2i(440,100);glVertex2i(440,80);
+  glEnd();
+  
+  glColor3f(0.0,0.0,0.0);
+  glBegin(GL_LINES);
+  glVertex2i(432,115);glVertex2i(437,125);
+  glVertex2i(438,115);glVertex2i(443,125);
+  glVertex2i(444,115);glVertex2i(449,125);
+  glEnd();
+  
+  //faixa pista
+  glColor3f(1.0, 1.0, 1.0);
+  glBegin(GL_LINES);
+  glVertex2i(250,0);  glVertex2i(250,50);
+  glVertex2i(250,100);  glVertex2i(250,150);
+  glVertex2i(250,180); glVertex2i(250, 200);
+  glEnd();
+
+  
+  glFlush();
+  glutSwapBuffers();
+ }*/
 
 void keyboard(unsigned char key, int x, int y){
   switch (key) {
